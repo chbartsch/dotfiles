616e36fdfc5d10420bc7c7e7c7322c022cd35dc0
chbartsch <22025013+chbartsch@users.noreply.github.com>
2018-06-06 21:15:59 +0200

    fixed bug regarding 'no tmux in remote session'

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,7 +1,7 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 # Try to run tmux only if we are *not* in a ssh remote session
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+if [ -z "$SSH_CLIENT" ] && [ -z "$SSH_TTY" ]; then
 	# Run tmux if exists
 	if command -v tmux>/dev/null; then
 		[ -z $TMUX ] && { tmux attach || exec tmux new-session; }
